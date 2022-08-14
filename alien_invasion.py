fb55ff5d67d9e14cdbc37bea7b22389717549cfa
MHoryd <michal.horyd@gmail.com>
2022-08-14 14:33:14 +0200

    add sip class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 ##Overall class to manage game assets and behavior
 class AlienInvasion:
@@ -10,19 +11,19 @@ class AlienInvasion:
         pygame.init()
         self.game_settings = Settings()
         self.screen = pygame.display.set_mode((self.game_settings.screen_width, self.game_settings.screen_height))
+        self.ship = Ship(self)
         pygame.display.set_caption("Alien Invasion")
-        ## set background color
 
     ## start main loop for a game
     def run_game(self):
         while True:
         ## listen for keyboart input
             for event in pygame.event.get():
-                print(event)
                 if event == pygame.QUIT:
                     sys.exit()
             ## Redraw the screen during each passing of the loop
             self.screen.fill(self.game_settings.bg_color)
+            self.ship.blitme()
             ## make the most recently refresh screen visible
             pygame.display.flip()
 
---
+++ ship.py
@@ -0,0 +1,18 @@
+import pygame
+
+## class to manage ship
+class Ship:
+    ## initialize ship and set it's starting position
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        ##load ship image and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        ## start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        ##draw ship at current loation
+        self.screen.blit(self.image, self.rect)

5438598fe8b9ad4ebfcc18dcda1c2999c509b3ab
MHoryd <michal.horyd@gmail.com>
2022-08-15 21:49:53 +0200

    Added Alien class, Fixed closing event issue

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 
 ##Overall class to manage game assets and behavior
 class AlienInvasion:
@@ -12,20 +13,31 @@ class AlienInvasion:
         self.game_settings = Settings()
         self.screen = pygame.display.set_mode((self.game_settings.screen_width, self.game_settings.screen_height))
         self.ship = Ship(self)
+        self.alien = Alien(self)
         pygame.display.set_caption("Alien Invasion")
 
     ## start main loop for a game
     def run_game(self):
         while True:
+            self._check_events()
+            self._update_screen()
+
+
+    def _check_events(self):
         ## listen for keyboart input
-            for event in pygame.event.get():
-                if event == pygame.QUIT:
-                    sys.exit()
-            ## Redraw the screen during each passing of the loop
-            self.screen.fill(self.game_settings.bg_color)
-            self.ship.blitme()
-            ## make the most recently refresh screen visible
-            pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def _update_screen(self):
+        ## Redraw the screen during each passing of the loop
+        self.screen.fill(self.game_settings.bg_color)
+        self.ship.blitme()
+        self.alien.blitme()
+
+        ## make the most recently refresh screen visible
+        pygame.display.flip()
 
 # make the game instance and run the game
 if __name__ == '__main__':

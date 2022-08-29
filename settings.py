7e2010d181e5209101caf9c53f01752b402ef21f
MHoryd <michal.horyd@gmail.com>
2022-08-29 22:56:58 +0200

    added buttelt file na class

--- settings.py
+++ settings.py
@@ -12,3 +12,8 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (50,50,230)
         self.ship_speed = 0.5
+        #Bullet setting
+        self.bullet_speed = 0.4
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_colour = (60,60,60)
--- ship.py
+++ ship.py
@@ -6,7 +6,6 @@ class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
         ##load ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
